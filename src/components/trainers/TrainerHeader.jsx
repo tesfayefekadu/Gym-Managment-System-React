8b7b02ad91ee7a3a8b71c3b93144645d983e68e1
Tesfaye Fekadu <fishfekadueg53@gmail.com>
2026-08-24 14:55:49 +0100

    trainer Header file added

--- src/components/trainers/TrainerHeader.jsx
+++ src/components/trainers/TrainerHeader.jsx
@@ -1,24 +1,24 @@
+import Button from "../common/Button";
+
 function TrainerHeader({ onAddTrainer }) {
   return (
     <div className="flex justify-between items-center mb-6">
-
       <div>
         <h1 className="text-3xl font-bold">
           Trainer Management
         </h1>
 
         <p className="text-gray-500">
-          Manage gym trainers.
+          Manage all gym trainers
         </p>
       </div>
 
-      <button
+      <Button
+        variant="primary"
         onClick={onAddTrainer}
-        className="bg-blue-600 hover:bg-blue-700 text-white px-5 py-3 rounded-lg"
       >
         + Add Trainer
-      </button>
-
+      </Button>
     </div>
   );
 }
